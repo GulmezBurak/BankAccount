25a03830a88b99799e06a4e2217e64b8bd8a3f0c
BURAK GULMEZ <burakgulmez34@gmail.com>
2022-10-02 17:59:02 +0300

    BankAccount
    BankAccount

---
+++ BankAccount/BankAccount/Costumer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    internal class Costumer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Password { get; set; }
+
+        public void CapitalLetter(string Name, string Surname)
+        {
+            Name = Name.ToUpper();
+            Surname = Surname.ToUpper();
+        }
+    }
+}
+        
---
+++ BankAccount/BankAccount/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    internal class Program
+    {
+        static List<Costumer> clist = new List<Costumer>();
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hoşgeldiniz Kullanıcı adınızı ve şifrenizi giriniz.");
+            Menu();
+        }
+
+
+    }
+}
